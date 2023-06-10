cd0c90a255c7a22a0514e9b59988dd223c69fe5f
Alon Gartenhaus <83038528+Alongarten@users.noreply.github.com>
2023-06-10 20:20:19 +0000

    slightly neater

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-/* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-// };
-
-// function that for loops each array, and returns concatenation of all 4.
-// then use foreach(?) on the function to return array.length number of times.
 
 window.onload = function() {
   //write your code here
@@ -24,15 +14,18 @@ let noun = ["jogger", "racoon", "website", "company", "adventure"];
 let extension = [".com", ".net", ".org", ".io", ".us"];
 
 function domainGenerator() {
-  let output = ""; // Variable to store the generated domains
+  let output = ""; 
   for (let a = 0; a < pronoun.length; a++) {
     for (let b = 0; b < adj.length; b++) {
       for (let c = 0; c < noun.length; c++) {
-        let domainHackGenerated = false; // Flag to check if domain hack is already generated
+        // check if domain hack (shorten) is already generated
+        let domainHackGenerated = false; 
         for (let d = 0; d < extension.length; d++) {
           let domain = pronoun[a] + adj[b] + noun[c] + extension[d];
           output += "<p>" + domain + "</p>";
 
+          // Set to true after generating the domain hack
+          
           if (noun[c].length > 3 && !domainHackGenerated) {
             let domainHack =
               pronoun[a] +
@@ -41,7 +34,7 @@ function domainGenerator() {
               "." +
               noun[c].slice(3);
             output += "<p>" + domainHack + "</p>";
-            domainHackGenerated = true; // Set the flag to true after generating the domain hack
+            domainHackGenerated = true; 
           }
         }
       }
@@ -49,10 +42,3 @@ function domainGenerator() {
   }
   document.querySelector("#domains").innerHTML = output;
 }
-
-// function domains(pronoun, adj, noun, extension) {
-//   let randPronoun;
-//   let randAdj;
-//   let randNoun;
-//   let randExtension;
-// }
